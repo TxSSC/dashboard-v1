83082d02c7a39f78ae9ba17c82411fbaf7215a49
Matt McFarland <matmcfarland@gmail.com>
2012-03-13 13:24:24 -0500

    refactored module builder

--- tasks/modules/index.js
+++ tasks/modules/index.js
@@ -2,16 +2,145 @@ var fs = require('fs'),
     path = require('path');
 
 
-task.registerBasicTask('modules', 'Compile modules into one file', function(data, name) {
+task.registerBasicTask('modules', 'Compile modules modules/build/', function(data, name) {
   var modules,
-      module_dir = path.resolve(data);
+      moduleDir = path.resolve(data),
+      cacheDir = path.join(moduleDir, 'tmp-build/'),
+      buildDir = path.resolve(config('buildDir') + '/modules');
 
-  modules = fs.readdirSync(module_dir).map(function(folder) {
-    return path.join(module_dir, folder);
+  /*
+   * Iterate over every module directory with the exception of
+   * tmp-build
+   */
+  modules = fs.readdirSync(moduleDir).map(function(folder) {
+    return path.join(moduleDir, folder);
   }).filter(function(thing) {
-    return fs.statSync(thing).isDirectory();
+    return fs.statSync(thing).isDirectory() &&
+            path.basename(thing) != 'tmp-build';
   });
 
-  console.dir(modules);
+  /*
+   * Build each module
+   */
+  modules.forEach(function(module) {
+    task.helper('modules', module);
+  });
 
+  //Error check on module builds
+  if(task.hadErrors()) {
+    return false;
+  }
+
+  /*
+   * Concat style/source/templates to build_dir/modules/ *.js/style.css/templates.js
+   */
+  var sources = file.expand(cacheDir + '/*.js'),
+      templates = file.expand(cacheDir + '/*.hogan'),
+      styles = file.expand(cacheDir + '/*.css');
+
+  /*
+   * Build js source files into one file and attach to window.modules
+   */
+  var jsSource = 'window.modules = window.modules || {};\n\n';
+  jsSource += sources.map(function(thing) {
+    return 'window.modules.' + path.basename(thing, '.js') +
+              ' = ' + file.read(thing);
+  }).join('\n').replace(/\n$/, '');
+  file.write(buildDir + '/module-main.js', jsSource);
+
+  /*
+   * Build templates into one files and attach to window.Templates
+   */
+  var templateSource = 'window.Templates = window.Templates || {};\n\n';
+  templateSource += templates.map(function(thing) {
+    return file.read(thing);
+  }).join('\n').replace(/\n$/, '');
+  file.write(buildDir + '/module-templates.js', templateSource);
+
+  /*
+   * Concat css to [buildDir]/module-styles.css
+   */
+  file.write(buildDir + '/module-styles.css', task.helper('concat', styles));
+
+
+  //Return from task
+  return task.hadErrors() ? false : true;
+});
+
+
+/*
+ * Helper that compiles the specified module
+ */
+task.registerHelper('modules', function(modulePath) {
+  var source = path.join(modulePath, 'module.coffee'),
+      style = path.join(modulePath, 'style.css'),
+      templates = file.expand(modulePath + '/templates/*.html'),
+      cacheDir = path.join(modulePath, '../tmp-build'),
+      moduleName = path.basename(modulePath);
+
+  /*
+   * Build source scripts
+   */
+  if(path.existsSync(source)) {
+    //Compile coffee-script
+    source = task.helper('coffee', [ source ], cacheDir)[0];
+
+    //Ensure coffee-script compiled
+    if(!source) {
+      log.error('Coffeescript did not compile for - ' + modulePath);
+      return false;
+    }
+
+    //Rename the compiled file to [moduleName].js
+    fs.renameSync(source, path.join(path.dirname(source), moduleName) + '.js');
+  }
+  else {
+    source = path.join(modulePath, 'module.js');
+
+    if(path.existsSync(source)) {
+      //Copy js
+      file.copy(source, path.join(cacheDir, 'module.js'));
+
+      //Rename the compiled file to [moduleName].js
+      fs.renameSync(path.join(cacheDir, 'module.js'), path.join(cacheDir, moduleName) + '.js');
+    }
+    else {
+      log.error('No sources found in - ' + modulePath);
+    }
+  }
+
+  /*
+   * Build templates
+   */
+  if(templates && templates.length) {
+    //Compile templates
+    file.write(path.join(cacheDir, moduleName) + '.hogan', task.helper('hogan', templates));
+  }
+
+  /*
+   * Build CSS
+   */
+  if(path.existsSync(style)) {
+    file.write(path.join(cacheDir, moduleName) + '.css', task.helper('mincss', [ style ]));
+  }
+
+  return true;
+});
+
+
+/*
+ * Cleans up the build directory under the modules
+ * directory.
+ * Requires the clean task to the present, and modules
+ */
+task.registerBasicTask('clean-modules', 'Cleans up the build dir in modules', function() {
+  var dir = path.resolve(config('modules').folder + '/tmp-build');
+
+  if(!dir) {
+    log.error('clean-modules directory is undefined, this could be bad.');
+  }
+  else {
+    console.dir('deleting: ' + dir);
+    //task.helper('clean', dir);
+  }
 });
