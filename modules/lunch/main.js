cb6b251bb5ef907e4383e2dbf40036da658aaadf
Matt McFarland <matmcfarland@gmail.com>
2012-04-02 11:42:24 -0500

    lunch is now a simple plot

--- modules/lunch/main.js
+++ modules/lunch/main.js
@@ -7,7 +7,14 @@
     url: '/lunch-proxy/lunch',
 
     initialize: function() {
+      var self = this;
+
       this.poll();
+      this.fetch({
+        success: function() {
+          self.trigger('fetch');
+        }
+      });
     },
 
     poll: function() {
@@ -29,50 +36,108 @@
 
     initialize: function() {
       this.model = new Lunch();
-      this.model.fetch();
 
-      //Make sure to re-render on change
-      this.model.on('change:day', this.render, this);
-      this.model.on('change:entities', this.renderEntities, this);
+      this.model.on('fetch', this.render, this);
     },
 
     render: function() {
       this.$el.html(Templates.lunch.base.render(this.model.toJSON()));
-      this.renderEntities();
+      if(!this.plot) this.renderPlot();
+
+      //Make sure to re-render on change
+      this.model.on('change:day', this.render, this);
+      this.model.on('change:entities', this.renderPlot, this);
 
       return this;
     },
 
-    renderEntities: function() {
-      var self = this,
-          things = this.model.get('entities');
-          voters = this.model.get('votees');
+    packageData: function() {
+      var data,
+          self = this;
 
-      $('.ranking', this.$el).fadeOut(100, function() {
-        $(this).empty();
+      data = this.model.get('entities').map(function(entity) {
+        return { name: entity.name, data: [ entity.rating ] };
+      });
 
-        _.chain(things)
-          .sortBy(function(thing) {
-            return -thing.rating;
-          }).each(function(thing) {
-            $('.ranking', self.$el)
-              .append(Templates.lunch.location.render(thing));
-          });
-      }).fadeIn(100);
-      $('.votees', this.$el).fadeOut(100, function() {
-        $(this).empty();
+      return data;
+    },
 
-        //Render voters
-        if((!self.model.isNew() && !voters.length) || (voters && !voters.length)) {
-          $('.votees', self.$el).append($('<li class="shadow">No voters yet!</li>'));
-        }
-        else {
-          _.each(voters, function(voter) {
-          $('.votees', self.$el)
-            .append($('<li class="shadow">' + voter + '</li>'));
-          });
-        }
-      }).fadeIn(100);
+    renderPlot: function() {
+      var self = this;
+
+      this.plot = new Highcharts.Chart({
+        credits: {
+          enabled: false
+        },
+        chart: {
+          type: 'column',
+          renderTo: $('.chart', self.$el).get(0),
+          plotShadow: false,
+          backgroundColor: 'transparent',
+          width: 220,
+          height: 160,
+          spacingTop: 0,
+          spacingRight: 0,
+          spacingBottom: 0,
+          spacingLeft: 0
+        },
+        title: {
+          text: self.model.get('day'),
+          style: {
+            color: '#EEE'
+          }
+        },
+        colors: [
+          '#50b432',
+          '#058DC7'
+        ],
+        xAxis: {
+          categories: [],
+          showFirstLabel: false,
+          title: {
+            text: 'Location',
+            style: {
+              'color': '#AAA',
+              'font-size': '14px',
+              'font-weight': 'normal'
+            }
+          }
+        },
+        yAxis: {
+          min: 0,
+          max: 8,
+          title: {
+            text: 'Rating',
+            style: {
+              'color': '#AAA',
+              'font-size': '14px',
+              'font-weight': 'normal'
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          backgroundColor: '#EFEFEF',
+          align: 'left',
+          verticalAlign: 'top',
+          x: 100,
+          y: 30,
+          floating: true,
+          shadow: true
+        },
+        tooltip: {
+          formatter: function() {
+            return this.y + ' votes';
+          }
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+          }
+        },
+        series: self.packageData()
+      });
 
       return this;
     }
