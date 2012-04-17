372bdeea3b4737ccf9f08d5179d035fa5e92d385
Matt McFarland <matmcfarland@gmail.com>
2012-04-17 16:01:42 -0500

    removed unneeded bootstrap code

--- api/sockets/lunch.js
+++ api/sockets/lunch.js
@@ -3,16 +3,7 @@ var http = require('http');
 module.exports = function(io) {
   var lunch = io
   .of('/lunch')
-  .on('connection', function(socket) {
-
-    /*
-     * emit the current day on connection
-     */
-    fetchDay(function(day) {
-      socket.emit('day:new', day);
-    });
-
-  });
+  .on('connection', function(socket) {});
 
   /*
    * Process the event and pass the `data` we want to
@@ -30,27 +21,3 @@ module.exports = function(io) {
    */
   return handler;
 };
-
-
-function fetchDay(callback) {
-  var options = {
-    host: process.env.LUNCH_HOST,
-    port: process.env.LUNCH_PORT,
-    path: '/lunch'
-  };
-
-  http.get(options, function(res) {
-    if(res.statusCode === 200) {
-      var json = '';
-
-      res.setEncoding('utf8');
-      res.on('data', function(chunk) {
-        json += chunk;
-      });
-
-      res.on('end', function() {
-        return callback(JSON.parse(json));
-      });
-    }
-  });
-}
